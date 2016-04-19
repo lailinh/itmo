85c5b36b3bf418a99729a1bcbb620dc6b346c19b
lailinh <linh.hotaru@gmail.com>
2016-04-19 12:29:54 +0200

    my first commit to GIT

---
+++ SuperTest/src/edu/itmo/git/SuperTestServlet.java
@@ -0,0 +1,12 @@
+package edu.itmo.git;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class SuperTestServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
